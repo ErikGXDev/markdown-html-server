27156829fffa1f35aff782985d4f7b6b25dbecec
ErikoXDev <54033728+ErikoXDev@users.noreply.github.com>
2023-04-01 19:22:11 +0200

    Release

---
+++ markdown.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // Make sure to re-run your repl/project after you changed the configuration!
+
+  // A markdown flavour changes the behaviour of your markdown
+  // Current flavours: original, vanilla, github
+  // More: https://github.com/showdownjs/showdown#flavors
+
+  // I personally recommend leaving this as "github" since it supports line breaks
+  FLAVOUR: "github",
+
+  // Allow visitors to visit /README.md
+  // false by default
+  ENABLE_README: false,
+
+
+
+}
